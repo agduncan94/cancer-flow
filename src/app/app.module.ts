374cd456b1a454ee4233db0f7adde14bcd30f70e
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-03-31 22:25:53 -0400

    add gene page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GenesComponent } from './genes/genes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    NavbarComponent
+    NavbarComponent,
+    GenesComponent
   ],
   imports: [
     BrowserModule,
